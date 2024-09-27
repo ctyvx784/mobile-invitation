70786a018eacc67a9a217d2ab0df69db5f7115aa
ctyvx784 <ctyvx784@gmail.com>
2024-09-27 22:09:33 +0900

    test

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,7 +4,14 @@ import { getDatabase } from 'firebase/database';
 // TODO: https://firebase.google.com/docs/web/setup?hl=ko
 // 가이드에 따라 firebase config를 설정합니다.
 const firebaseConfig = {
-
+  apiKey: "AIzaSyD3dH6MHXy_vqCVn1IAIkgt0gAzcjDLpfw",
+  authDomain: "issac-kang.firebaseapp.com",
+  databaseURL: "https://issac-kang-default-rtdb.firebaseio.com",
+  projectId: "issac-kang",
+  storageBucket: "issac-kang.appspot.com",
+  messagingSenderId: "682836274910",
+  appId: "1:682836274910:web:461b304964377903429c4f",
+  measurementId: "G-R2GKDDW5TZ"
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
--- src/layout/Gallery/Images.ts
+++ src/layout/Gallery/Images.ts
@@ -1,77 +1,54 @@
-import image01 from '@/assets/images/01.jpg'
-import image02 from '@/assets/images/02.jpg'
-import image03 from '@/assets/images/03.jpg'
-import image04 from '@/assets/images/04.jpg'
-import image05 from '@/assets/images/05.jpg'
-import image06 from '@/assets/images/06.jpg'
-import image07 from '@/assets/images/07.jpg'
-import image08 from '@/assets/images/08.jpg'
-import image09 from '@/assets/images/09.jpg'
-import image10 from '@/assets/images/10.jpg'
-import image11 from '@/assets/images/11.jpg'
-import image12 from '@/assets/images/12.jpg'
-
 const images = [   /* 이미지 경로를 리스트로 저장 */
   {
     alt: 'image01',
-    source: image01,
+    source: "https://firebasestorage.googleapis.com/v0/b/issac-kang.appspot.com/o/IMG_1599.jpg?alt=media&token=920b1fb6-7452-48a9-ae36-9ecfec576ebd",
     width: 640,
     height: 960
   },
   {
     alt: 'image02',
-    source: image02,
+    source: "https://firebasestorage.googleapis.com/v0/b/issac-kang.appspot.com/o/IMG_1224.jpg?alt=media&token=88d6e203-3627-49d4-9f31-1cce1ae36ddb",
     width: 640,
     height: 960
   },
   {
     alt: 'image03',
-    source: image03,
+    source: "https://firebasestorage.googleapis.com/v0/b/issac-kang.appspot.com/o/IMG_1353.jpg?alt=media&token=a37f384a-c36c-4b1e-93f1-3b827bded06b",
     width: 640,
     height: 960
   },{
     alt: 'image04',
-    source: image04,
+    source: "https://firebasestorage.googleapis.com/v0/b/issac-kang.appspot.com/o/IMG_1392.jpg?alt=media&token=de315b8a-8894-4b31-9a93-e8895310530c",
     width: 640,
     height: 960
   },{
     alt: 'image05',
-    source: image05,
+    source: "https://firebasestorage.googleapis.com/v0/b/issac-kang.appspot.com/o/IMG_1454.jpg?alt=media&token=8cee785e-d695-4528-b2ce-d428577aff2c",
     width: 640,
     height: 960
   },{
     alt: 'image06',
-    source: image06,
+    source: "https://firebasestorage.googleapis.com/v0/b/issac-kang.appspot.com/o/IMG_1464.jpg?alt=media&token=079a6de2-f725-42cd-84cd-1f99da1bdf1a",
     width: 640,
     height: 960
   },{
     alt: 'image07',
-    source: image07,
+    source: "https://firebasestorage.googleapis.com/v0/b/issac-kang.appspot.com/o/IMG_1470.jpg?alt=media&token=19677a12-1c38-46c3-a23c-6d7975556b52",
     width: 640,
     height: 960
   },{
     alt: 'image08',
-    source: image08,
+    source: "https://firebasestorage.googleapis.com/v0/b/issac-kang.appspot.com/o/IMG_1574.jpg?alt=media&token=d6753a17-757c-42c1-bd0f-df9091d7c667",
     width: 640,
     height: 960
   },{
     alt: 'image09',
-    source: image09,
+    source: "https://firebasestorage.googleapis.com/v0/b/issac-kang.appspot.com/o/IMG_1091.jpg?alt=media&token=c319ab6b-f81a-4d3e-868e-76437233828c",
     width: 640,
     height: 960
   },{
     alt: 'image10',
-    source: image10,
-    width: 640,
-    height: 960
-  },{
-    alt: 'image11',
-    source: image11,
-    width: 640,
-    height: 960
-  },{
-    alt: 'image12',
-    source: image12,
+    source: "https://firebasestorage.googleapis.com/v0/b/issac-kang.appspot.com/o/IMG_1673.jpg?alt=media&token=55bf62ea-bab6-409a-84bc-e5fa5e9f76d0",
     width: 640,
     height: 960
   }
